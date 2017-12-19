548d10449ee5b5b6b69feddc4d723a01ee16887e
kokil87 <33960055+kokil87@users.noreply.github.com>
2017-12-19 23:47:12 +0530

    Add files via upload

--- php/createtableuserid.php
+++ php/createtableuserid.php
@@ -7,7 +7,14 @@
 		die("ERROR") ;
 	}
 
-	$sql = "CREATE TABLE UserId;";	
+	$sql = "CREATE TABLE UserId(
+	userEnrollment int(6) unsigned NOT NULL PRIMARY KEY ,
+	userName varchar(30) NOT NULL ,
+	userId varchar(15) UNIQUE NOT NULL,
+	userPassword varchar(20) NOT NULL ,
+	userDescription varchar(50)  
+
+	);";	
 	
 	if($connection->query($sql) !== TRUE){
 		die("ERROR") ;
